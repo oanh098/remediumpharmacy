c298f920839cf3a792c7f3454f7b947c5954593a
OANH TRAN <45414817+oanh098@users.noreply.github.com>
2020-02-06 08:39:50 +0700

    2020Feb6 add vendor folder

--- public/js/app.js
+++ public/js/app.js
@@ -19335,8 +19335,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\WORKING\mastercatchall001\remediumbio\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\WORKING\mastercatchall001\remediumbio\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\WORKING\mastercatchall001\remediumpharmacy\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\WORKING\mastercatchall001\remediumpharmacy\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
